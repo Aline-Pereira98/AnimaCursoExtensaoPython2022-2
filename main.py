915b5068707f1ad1ecf1a4704787cd92e62f5e45
Aline Pereira <91352779+Aline-Pereira98@users.noreply.github.com>
2022-10-20 01:22:08 +0000

    Novo

---
+++ main.py
@@ -0,0 +1,11 @@
+# Meu primeiro projeto python
+print('Hello World!')
+#Não precisa declarar numero como int pq ele ja entende que é int,float
+nome = "Aline Pereira"
+idade = 24
+#Não da para concatenar uma str com int,então tem que converter o int 
+print('minha idade é',str(idade))
+#variações de usar
+print(f'minha idade é {idade}')
+print('minha idade é {}'.format(idade))
+print('meu nome é {} e tenho {} anos'.format(nome,idade))
